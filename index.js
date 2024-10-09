b57cfdfaea32f5c39fff55c07c54937920e3680c
Ankitk0362 <guptaankitonly4u@gmail.com>
2024-10-09 23:29:57 +0530

    feat: Copy qoute and share on twitter

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const quote = document.getElementById("quote");
 const author = document.getElementById("author");
 const btn = document.getElementById("btn");
 
+const copyBtn = document.getElementById('copyBtn');
+ const tweetBtn = document.getElementById('tweetBtn');
+
 btn.addEventListener("click", getQuote);
  getQuote();
 function getQuote() {
@@ -14,3 +17,20 @@ function getQuote() {
       author.innerHTML = `- ${data.author}`;
     });
 }
+
+
+copyBtn.addEventListener('click', () => {
+  const quoteToCopy = `${quote.innerHTML} ${author.innerHTML}`;
+  navigator.clipboard.writeText(quoteToCopy).then(() => {
+      alert("Quote copied to clipboard!");
+  }).catch(err => {
+      alert("Failed to copy quote.");
+  });
+});
+
+
+tweetBtn.addEventListener('click', () => {
+  const tweetText = `${quote.innerHTML} ${author.innerHTML}`;
+  const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}`;
+  window.open(tweetUrl, '_blank');
+}); 
